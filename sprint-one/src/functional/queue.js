b1295b46a38ad44b46e7e7d7a00e73bb4452cc22
veeweeherman <vy.hoang.cu@gmail.com>
2015-07-01 12:22:09 -0700

    functional pattern stack completes, most of queue shared beh' completed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,28 @@
 var Queue = function(){
   var someInstance = {};
-
+  var size = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    size++;
+    return size;
   };
 
   someInstance.dequeue = function(){
+    delete storage[size-1]
+
+    if (size > 0) {
+    size--;
+    }
+    return size
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,30 @@
 var Stack = function(){
   var someInstance = {};
+  var size = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function(){
+    //The pop() method removes the last element from an array and returns that element.
+    var result = storage[size-1] //last item
+    delete storage[size-1] //removing last item
+
+    if (size > 0) {
+      size--;
+    }
+    console.log(result)
+    return result;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
